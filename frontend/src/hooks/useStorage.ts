0e2b6e1fc9b7abdf7ed90aa40f35f73c65807b34
Samila Vieira <samilav1997@gmail.com>
2023-07-11 13:07:17 -0300

    corrigindo bug

--- frontend/src/hooks/useStorage.ts
+++ frontend/src/hooks/useStorage.ts
@@ -1,8 +1,8 @@
 import React from "react";
 
-export const useStorage = <T,>(key: string, initialValue: T) => {
+export const useStorage = <T>(key: string, initialValue: T) => {
   const [value, setValue] = React.useState<T>(() => {
-    return JSON.parse(localStorage.getItem(key) || "") || initialValue;
+    return JSON.parse(localStorage.getItem(key) || "{}") || initialValue;
   });
 
   React.useEffect(() => {
